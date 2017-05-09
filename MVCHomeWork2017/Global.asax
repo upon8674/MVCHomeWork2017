e4393a3acdc9e559c2db5d6246280b007c73dd9b
uponl <uponl@cnyes.com>
2017-05-09 18:44:42 +0800

    加入專案檔案。

---
+++ MVCHomeWork2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCHomeWork2017.MvcApplication" Language="C#" %>
---
+++ MVCHomeWork2017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCHomeWork2017.Startup))]
+namespace MVCHomeWork2017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
