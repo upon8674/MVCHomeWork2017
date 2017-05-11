c211d366ede49e0a0f2c5c150661d26bef0856d2
upon <upon8674@gmail.com>
2017-05-12 01:33:57 +0800

    新增email驗證

--- MVCHomeWork2017/Models/客戶資料.Partial.cs
+++ MVCHomeWork2017/Models/客戶資料.Partial.cs
@@ -33,6 +33,8 @@ namespace MVCHomeWork2017.Models
         public string 地址 { get; set; }
         
         [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+        [Required]
+        [EmailAddress(ErrorMessage = "信箱格式錯誤")]
         public string Email { get; set; }
     
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
